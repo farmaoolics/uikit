110fbcd93d97397371a2fe2462ae98e7e5b21390
danielfpaiva <danielfonsecapaiva@gmail.com>
2021-07-31 01:38:03 +0100

    change gradient

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -60,7 +60,7 @@ export const darkColors: Colors = {
   textSubtle: "#AEAEAE", // menu text
   borderColor: "#524B63",
   gradients: {
-    bubblegum: "linear-gradient(#DEF7DE 43%,#DEF7DE 100%)",
+    bubblegum: "linear-gradient(#fff 43%,#fff 100%)",
     cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
     blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
     violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
